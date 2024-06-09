02e1d3ae40151b9c15c0dec44caa644711f541b9
Yakshit Jain <jainyakshit1@gmail.com>
2024-06-09 17:21:43 +0530

    updated changes - 2024-06-09 17:21:43

---
+++ app/src/main/java/com/crio/xnews/NewsApiResponse.java
@@ -0,0 +1,14 @@
+package com.crio.xnews;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsApiResponse {
+    private String status;
+    private int totalResults;
+    private List<NewsArticle> articles = new ArrayList<>();
+
+// TODO: CRIO_TASK_MODULE_PROJECT
+// Please define all the methods required here as mentioned in the XNews BuildOut Milestone for each functionality before implementing the logic.
+// This will ensure that the project can be compiled successfully.
+}
